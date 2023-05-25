dfcc7f00b95fac32a910af7f2c001c4896d7b285
HliasOuzounis <iliouzoun@outlook.com>
2023-05-25 19:04:09 +0300

    changed directories to match new folders

--- analyze_game.py
+++ analyze_game.py
@@ -25,35 +25,166 @@ def fen_to_bitboard(fen_str):
     return torch.cat((torch.tensor([1 if move == "w" else -1]), bitboard.flatten()))
 
 
-def analyze_game(game, engine, device):
-    mate_score = 9999
+def pad_tensor(tensor, length, pad_value):
+    return torch.cat((tensor, torch.ones(length - len(tensor)) * pad_value))
+
+def analyze_game(game, engine, mate_score, nof_moves=10):
     board = game.board()
     analysis = []
     
     moves = list(game.mainline_moves())
-    total_moves = len(moves)
-    for i in tqdm(range(total_moves), desc="Analyzing game"):
+    # for i, move in enumerate(game.mainline_moves()):
+    for i in tqdm(range(len(moves)), desc="Analyzing game"):
         move = moves[i]
-        pad_value = -9999 if board.turn else 9999
-        
-        board_position_tensor = fen_to_bitboard(board.fen())
-        
-        # top_moves = get_top_moves(board, nof_moves)
-        # top_moves_tensor = pad_tensor(torch.tensor([move[1] for move in top_moves]), nof_moves, pad_value)
-        best_move_tensor = torch.tensor([engine.analyse(board, chess.engine.Limit(time=0.1))["score"].white().score(mate_score=mate_score)])
-        
+        # Enfine evaluation before the move
+        top_moves = engine.analyse(board, chess.engine.Limit(time=0.1), multipv=nof_moves)
+        # A tensor with the score of the top nof_moves moves (normalized to be between -1 and 1)
+        top_moves_tensor = torch.Tensor([eval["score"].relative.score(mate_score=mate_score) for eval in top_moves]) 
+        # Pad the tensor if there are less than nof_moves legal moves
+        top_moves_tensor = pad_tensor(top_moves_tensor, nof_moves, -mate_score) / mate_score
+        # Win, draw, loss chance tensor before the move
+        before_wdl = top_moves[0]["score"].relative.wdl()
+        before_wdl_tensor = torch.Tensor([
+            before_wdl.winning_chance(), 
+            before_wdl.drawing_chance(), 
+            before_wdl.losing_chance()
+        ])
         board.push(move)
         
-        after_move_tensor = torch.tensor([engine.analyse(board, chess.engine.Limit(time=0.1))["score"].white().score(mate_score=mate_score)])
+        # Engine evaluation after the move
+        after_move = engine.analyse(board, chess.engine.Limit(time=0.1))
+        # Now it's the opponent's turn so negate the score
+        after_move_tensor = torch.Tensor([after_move["score"].relative.score(mate_score=mate_score) * -1]) / mate_score
+        # Reverse the list so that it's from the perspective of the player who just moved
+        after_wdl = after_move["score"].relative.wdl()
+        after_wdl_tensor = torch.Tensor([
+            after_wdl.losing_chance(), 
+            after_wdl.drawing_chance(), 
+            after_wdl.winning_chance()
+        ])
 
-        # analysis.append(torch.cat((board_position_tensor, top_moves_tensor, after_move_tensor)))
-        analysis.append(torch.cat((board_position_tensor, best_move_tensor, after_move_tensor)))
+        analysis.append(torch.cat((
+            top_moves_tensor, before_wdl_tensor, 
+            after_move_tensor, after_wdl_tensor
+        )))
+        # print(f"before move: {move}, top_moves {top_moves_tensor}, wdl {before_wdl_tensor}")
+        # print(f"after:               top_moves {after_move_tensor}, wdl {after_wdl_tensor}")
     
-    # Pad the game if it ends on white's turn to batch white's and black's analysis together later
+    # Pad the game if it ends on white's turn to batch white's and black's analysis together
     if len(analysis) % 2:
-        analysis.append(torch.zeros_like(analysis[0]))
+        analysis.append(torch.ones_like(analysis[0]) * (-mate_score))
     
     white_analysis = torch.stack([position for position in analysis[::2]])
     black_analysis = torch.stack([position for position in analysis[1::2]])
     
-    return torch.stack((white_analysis, black_analysis)).to(device)
+    return torch.stack((white_analysis, black_analysis))
+
+
+def convert_position(game, func):
+    board = game.board()
+    positions = []
+    
+    for move in game.mainline_moves():
+        board_position_tensor = func(board.fen())
+        positions.append(board_position_tensor)
+        board.push(move)
+        
+    # Pad the game if it ends on white's turn to batch white's and black's analysis together later
+    if len(positions) % 2:
+        positions.append(torch.zeros_like(positions[0]))
+    
+    white_positions = torch.stack([position for position in positions[::2]])
+    black_positions = torch.stack([position for position in positions[1::2]])
+    
+    return torch.stack((white_positions, black_positions))
+
+def fen_to_bitboard(fen_str):
+    bitboard = torch.zeros(12, 64)
+    fen, move, _castle, _en_passant, _halfmove, _fullmove = fen_str.split(" ")
+    
+    mapings = {
+        "P": 0, "N": 1, "B": 2, "R": 3, "Q": 4, "K": 5,
+        "p": 6, "n": 7, "b": 8, "r": 9, "q": 10, "k": 11,
+    }
+    
+    row, col = 0, 0
+    for char in fen:
+        if char == "/":
+            row += 1
+            col = 0
+        elif char.isdigit():
+            col += int(char)
+        else:
+            bitboard[mapings[char], row * 8 + col] = 1
+            col += 1
+    # Flatten the bitboard and add whose move it is
+    return torch.cat((torch.tensor([1 if move == "w" else -1]), bitboard.flatten()))
+
+def fen_to_bitboard_mirror(fen_str):
+    bitboard = torch.zeros(12, 64)
+    fen, move, _castle, _en_passant, _halfmove, _fullmove = fen_str.split(" ")
+    
+    mapings = {
+        "P": 0, "N": 1, "B": 2, "R": 3, "Q": 4, "K": 5,
+        "p": 6, "n": 7, "b": 8, "r": 9, "q": 10, "k": 11,
+    }   if move == "w" else {
+        "p": 0, "n": 1, "b": 2, "r": 3, "q": 4, "k": 5,
+        "P": 6, "N": 7, "B": 8, "R": 9, "Q": 10, "K": 11,
+    } 
+    fen = fen if move == "w" else fen[::-1]
+    row, col = 0, 0
+    for char in fen:
+        if char == "/":
+            row += 1
+            col = 0
+        elif char.isdigit():
+            col += int(char)
+        else:
+            bitboard[mapings[char], row * 8 + col] = 1
+            col += 1
+    # Flatten the bitboard and add whose move it is
+    return torch.cat((torch.tensor([1 if move == "w" else -1]), bitboard.flatten()))
+
+def fen_to_board(fen_str):
+    board = torch.zeros(8, 8)
+    fen, move, _castle, _en_passant, _halfmove, _fullmove = fen_str.split(" ")
+    normalizer = 20
+    mapings = {
+        "P": 1, "N": 3, "B": 3.5, "R": 5, "Q": 9, "K": 20,
+        "p": -1, "n": -3, "b": -3.5, "r": -5, "q": -9, "k": -20,
+    } 
+    row, col = 0, 0
+    for char in fen:
+        if char == "/":
+            row += 1
+            col = 0
+        elif char.isdigit():
+            col += int(char)
+        else:
+            board[row, col] = mapings[char] / normalizer
+            col += 1
+    return torch.cat((torch.tensor([1 if move == "w" else -1]), board.flatten())) 
+
+def fen_to_board_mirror(fen_str):
+    board = torch.zeros(8, 8)
+    fen, move, _castle, _en_passant, _halfmove, _fullmove = fen_str.split(" ")
+    normalizer = 20
+    mapings = {
+        "P": 1, "N": 3, "B": 3.5, "R": 5, "Q": 9, "K": 20,
+        "p": -1, "n": -3, "b": -3.5, "r": -5, "q": -9, "k": -20,
+    } if move == "w" else {
+        "P": -1, "N": -3, "B": -3.5, "R": -5, "Q": -9, "K": -20,
+        "p": 1, "n": 3, "b": 3.5, "r": 5, "q": 9, "k": 20,
+    }
+    fen = fen if move == "w" else fen[::-1]
+    row, col = 0, 0
+    for char in fen:
+        if char == "/":
+            row += 1
+            col = 0
+        elif char.isdigit():
+            col += int(char)
+        else:
+            board[row, col] = mapings[char] / normalizer
+            col += 1
+    return torch.cat((torch.tensor([1 if move == "w" else -1]), board.flatten())) 
--- guess_the_elo.py
+++ guess_the_elo.py
@@ -1,11 +1,29 @@
+import argparse
+
 import chess.engine
 import chess.pgn
 import torch
 
 from models.lstm_network import LSTM
-from analyze_game import analyze_game
+from analyze_game import (
+    analyze_game, convert_position,
+    fen_to_bitboard, fen_to_bitboard_mirror,
+    fen_to_board, fen_to_board_mirror
+)
 
-import argparse
+
+position_converters = {
+    "bitboards": fen_to_bitboard,
+    "bitboards_mirrors": fen_to_bitboard_mirror,
+    "boards": fen_to_board,
+    "boards_mirrors": fen_to_board_mirror,
+}
+# position_type = "bitboards"
+# position_type = "bitboards_mirrors"
+# position_type = "boards"
+position_type = "boards_mirrors"
+
+mate_score = 1_000
 
 
 def get_game():
@@ -21,23 +39,17 @@ def get_game():
         "-c", action="store_true", help="True if the pgn file is from chess.com false otherwise"
     )
     args = parser.parse_args()
-    
+
     return args.pgn_file, args.engine, args.c
 
 # Based on probability distribution per rating range
 # Try to guess the mean elo of the player
+
+
 def guess_elo_range(elo_range):
     rating_ranges = [
-        (400, 900),
-        (900, 1100),
-        (1100, 1300),
-        (1300, 1500),
-        (1500, 1700),
-        (1700, 1900),
-        (1900, 2100),
-        (2100, 2300),
-        (2300, 2500),
-        (2500, 3000),
+        (800, 1000), (1000, 1200), (1200, 1400), (1400, 1600), (1600, 1800),
+        (1800, 2000), (2000, 2200), (2200, 2400), (2400, 2600), (2600, 3000)
     ]
     s = 0
     for r, probability in zip(rating_ranges, elo_range):
@@ -50,43 +62,61 @@ def convert_to_chessdotcom(prediction):
     # Lichess ratings are usually around 400 points higher than chess.com
     return prediction - 400
 
+
 def main():
     game_path, engine_path, is_chessdotcom = get_game()
     # game_path, engine_path, is_chessdotcom = "hlias_game.pgn", "/usr/bin/stockfish", False
     game = chess.pgn.read_game(open(game_path))
     if game is None:
         raise Exception("Not a valid pgn file")
-    
+
     engine = chess.engine.SimpleEngine.popen_uci(engine_path)
-    
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     device = torch.device("cpu")
-    analysis = analyze_game(game, engine, device)
 
-    input_size = analysis.shape[-1]
+    position = convert_position(game, func=position_converters[position_type])
+    analysis = analyze_game(game, engine, mate_score).to(device)
+    model_input = torch.cat((position, analysis), dim=-1)
 
+    engine.close()
+
+    input_size = position.shape[-1] + analysis.shape[-1]
+
+    predict_single_output(is_chessdotcom, device, model_input, input_size)
+
+    predict_rating_ranges(is_chessdotcom, device, model_input, input_size)
+
+
+def predict_single_output(is_chessdotcom, device, model_input, input_size):
     max_elo = 3000
     single_output_model = LSTM(input_size, 128, 2, device, 1)
-    single_output_model.load_state_dict(torch.load("models/lstm_model_single_output.pt"))
-    prediction = single_output_model(analysis, train=False)[0] * max_elo
+    single_output_model.load_state_dict(
+        torch.load(f"models/single_output/{position_type}.pt")
+    )
+    prediction = single_output_model(model_input, train=False)[0] * max_elo
 
     if is_chessdotcom:
         prediction = convert_to_chessdotcom(prediction)
+        
     print(f"Model1 prediction: {int(prediction[0].item())} for white and {int(prediction[1].item())} for black")
-    
+
+
+def predict_rating_ranges(is_chessdotcom, device, model_input, input_size):
     classes = 10
     rating_ranges_model = LSTM(input_size, 128, 2, device, classes)
-    rating_ranges_model.load_state_dict(torch.load("models/lstm_model_rating_ranges.pt"))
-    prediction_w, prediction_b = rating_ranges_model(analysis, train=False)[0]
-    prediction_w = guess_elo_range(prediction_w)
-    prediction_b = guess_elo_range(prediction_b)
-    
+    rating_ranges_model.load_state_dict(
+        torch.load(f"models/rating_ranges/{position_type}.pt")
+    )
+    prediction = rating_ranges_model(model_input, train=False)
+    prediction_w, prediction_b = tuple(guess_elo_range(pred) for pred in prediction[0])
+
     if is_chessdotcom:
         prediction_w = convert_to_chessdotcom(prediction_w)
         prediction_b = convert_to_chessdotcom(prediction_b)
+        
     print(f"Model2 prediction: {prediction_w} for white and {prediction_b} for black")
-    
-    engine.close()
-    
+
+
 if __name__ == "__main__":
     main()
